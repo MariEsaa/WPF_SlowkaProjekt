05b7f571e44cebe3240993b4ae350b5bd1a54080
MlodyTabaluga <mszubryt2005@gmail.com>
2024-11-12 12:46:04 +0100

    utworz progrss bar

---
+++ WPF_SlowkaProjekt/ProgressBar.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_SlowkaProjekt
+{
+    /// <summary>
+    /// Logika interakcji dla klasy ProgressBar.xaml
+    /// </summary>
+    public partial class ProgressBar : Window
+    {
+        public ProgressBar()
+        {
+            InitializeComponent();
+        }
+
+        private void IncreaseProgress(object sender, RoutedEventArgs e)
+        {
+            if (UserProgressBar.Value < UserProgressBar.Maximum)
+            {
+                UserProgressBar.Value += 5; 
+                ProgressText.Text = $"{UserProgressBar.Value}% ukończono";
+            }
+        }
+
+        private void ResetProgress(object sender, RoutedEventArgs e)
+        {
+            UserProgressBar.Value = 0; 
+            ProgressText.Text = "0% ukończono";
+        }
+    }
+}
