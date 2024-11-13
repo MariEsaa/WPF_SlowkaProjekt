d42805cef859dda47635d80b84edf4d96d049af7
Paweł Szewczyk <pawelgg205@gmail.com>
2024-11-13 10:09:39 +0100

    obsluga bazy danych

---
+++ WPF_SlowkaProjekt/ClassModel/CategoryModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_SlowkaProjekt.ClassModel
+{
+    internal class CategoryModel
+    {
+        public string Category { get; set; }
+        public List<WordModel> Words { get; set; }
+        public int WordID { get; set; }
+    }
+}
---
+++ WPF_SlowkaProjekt/ClassModel/WordModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_SlowkaProjekt.ClassModel
+{
+    internal class WordModel
+    {
+        public string Word { get; set; }
+        public string  WordTranslated { get; set; }
+        public CategoryModel Category { get; set; }
+    }
+}
