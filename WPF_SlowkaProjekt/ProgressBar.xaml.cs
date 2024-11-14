07e77d0b28a3df06bebf1d435a4c06f905570a20
MlodyTabaluga <mszubryt2005@gmail.com>
2024-11-14 20:17:28 +0100

    Add return button

--- WPF_SlowkaProjekt/ProgressBar.xaml.cs
+++ WPF_SlowkaProjekt/ProgressBar.xaml.cs
@@ -23,5 +23,14 @@ namespace WPF_SlowkaProjekt
         {
             InitializeComponent();
         }
+
+        private void GoToMainWindow_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+
+
     }
 }
